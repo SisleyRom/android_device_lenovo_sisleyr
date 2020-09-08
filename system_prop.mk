aaa85c096adca38d324b78d8f21b6ae496ac6332
ma3axata <v.radulevich@gmail.com>
2020-09-08 22:24:14 +0300

    sisleyr: Move props to common

--- system_prop.mk
+++ system_prop.mk
@@ -11,10 +11,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=320
 
-# Factory Reset Protection
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.frp.pst=/dev/block/bootdevice/by-name/config
-
 # First api level, device has been commercially launched
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.product.first_api_level=21
@@ -23,11 +19,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.product.model.platform=1
 
-# Radio
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.radio.multisim.config=dsds \
-    persist.radio.custom_ecc=1 \
-    persist.radio.ecc_hard_1=998 \
-    persist.radio.ecc_hard_count=1 \
-    rild.libpath=/system/vendor/lib64/libril-qc-qmi-1.so \
-    ro.telephony.default_network=9,1
